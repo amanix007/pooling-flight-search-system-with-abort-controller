e28400fda3645a04a51e35ff386d8c8ad8f11140
Aman Ullah <amanix007@gmail.com>
2025-07-07 11:21:45 +0600

    addeed tw

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  // In v4, most configuration is done via CSS
+}
