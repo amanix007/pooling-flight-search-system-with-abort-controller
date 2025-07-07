0f61479785c5bef32c89135d84960122e581ec14
Aman Ullah <amanix007@gmail.com>
2025-07-07 10:45:29 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+export default function App() {
+  return (
+    <div>
+
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <div>
+  <div className="bg-sky-50"></div>
+  <div className="bg-sky-100"></div>
+  <div className="bg-sky-200"></div>
+  <div className="bg-sky-300"></div>
+  <div className="bg-sky-400"></div>
+  <div className="bg-sky-500"></div>
+  <div className="bg-sky-600"></div>
+  <div className="bg-sky-700"></div>
+  <div className="bg-sky-800"></div>
+  <div className="bg-sky-900"></div>
+  <div className="bg-sky-950"></div>
+</div>
+    </div>
+  )
+}
